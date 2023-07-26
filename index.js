472fa7216cfb036cc3148f57779a31e601d7035e
hipnos1606 <joannmurillo81@gmail.com>
2023-07-26 11:52:44 -0600

    setting config files dir on env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require('./src/app.js');
 const Database = require('./API_modules/Database/index.js');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 Database.getInstance();
 //const ExpiredArticlesNotifier = require('./API_modules/Articles/ExpiredArticlesNotifier.js');
 //ExpiredArticlesNotifier.getInstance();
@@ -8,9 +8,8 @@ const port = process.env.PORT || 800;
 
 const fbAdmin = require('firebase-admin');
 const fbApp = require('./firebase/app');
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
-const GoogleAppCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-const serviceAccount = require(GoogleAppCredentials);
+const firebaseConfig = require(process.env.FIREBASE_CONFIG);
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 const fb = fbApp.initializeApp({
   credential: fbAdmin.credential.cert(serviceAccount),
